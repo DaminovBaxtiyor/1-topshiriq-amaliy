457ece92dc3d5487477e2d224303f3f5e01cc7c6
DaminovBaxtiyor <161870138+DaminovBaxtiyor@users.noreply.github.com>
2024-03-12 15:32:12 +0000

    Create ikkinchi_masala.dart

---
+++ ikkinchi_masala.dart
@@ -0,0 +1,9 @@
+void main() {
+  int kitob1Bet = 126;
+  int kitob2Bet = 96;
+  int kitob3Bet = 114;
+
+  double ortachaBetlar = (kitob1Bet + kitob2Bet + kitob3Bet) / 3;
+
+  print('O\'rtacha betlar: $ortachaBetlar');
+}
